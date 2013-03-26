7a425e0313db60a9dbaf8d3d090ca1c2b953ced4
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-03-26 15:38:45 +0100

    Removing line feed at the front of the lines

--- main.cpp
+++ main.cpp
@@ -15,13 +15,24 @@
 sigjmp_buf sigalarm_context;
 
 void sigalarm_handler(int unused);
+void exitWell(int status, std::string message);
 int main();
 std::string device;
 unsigned int deviceId;
+std::ofstream outputFile;
 
 void sigalarm_handler(int unused)
 	{
 	siglongjmp(sigalarm_context, 1);
+	}
+
+void exitWell(int status, std::string message)
+	{
+	syslog(LOG_INFO, std::string(device+": "+message).c_str());
+	if(outputFile.is_open())
+		outputFile.close();
+	closelog();
+	exit(status);
 	}
 
 int main()
@@ -40,7 +51,7 @@ int main()
 		while(1)
 			{
 			unsigned char buffer[8];
-			alarm(1200);
+			alarm(200);
 			// Reading the header
 			//read (0, buffer, sizeof buffer);
 			fread (buffer, (size_t) 1, sizeof buffer, stdin);
@@ -54,7 +65,7 @@ int main()
 			sbuffer[5]=(char) buffer[5];
 			sbuffer[6]=(char) buffer[6];
 			sbuffer[7]=(char) buffer[7];
-			sbuffer[8]=NULL;
+			sbuffer[8]=(char) NULL;
 			// Test if buffer is a sync header
 			if(buffer[1] == (unsigned char) 0xF8&&buffer[0] == (unsigned char) 0xFA)
 				{
@@ -74,7 +85,6 @@ int main()
 				std::stringstream ss;
 				ss << "Device id is '" << deviceId << "', written " << written << " bytes.";
 				syslog(LOG_INFO, std::string(ss.str()).c_str());
-				exit(EXIT_SUCCESS);
 				continue;
 				}
 			// it's a line
@@ -89,28 +99,24 @@ int main()
 				std::string line;
 				line.append(std::string(sbuffer));
 				line.append(curline);
+				while((!line.empty())&&(line[0]=='\r'||line[0]=='\n'))
+					line.erase(0,1);
 				syslog(LOG_INFO, std::string("First line is '"+line+"'.").c_str());
 				std::size_t pos;
-				std::ofstream outputFile;
 				// Finding device name
 				if(device.empty())
 					{
 					pos=line.find(',');
 					if(pos==std::string::npos)
 						{
-						syslog(LOG_INFO, std::string("No comma found in '"+line+"', case 1.").c_str());
-						closelog();
-						exit(EXIT_FAILURE);
+						exitWell(EXIT_FAILURE,"No comma found in '"+line+"', case 1.");
 						}
 					device=std::string(line, 0, pos);
 					syslog(LOG_INFO, std::string("Device is '"+device+"', cought from a line.").c_str());
 					// Testing device name
 					if(device.length()<5)
 						{
-						syslog(LOG_INFO, std::string("Bad device name.").c_str());
-						closelog();
-						exit(EXIT_FAILURE);
-						continue;
+						exitWell(EXIT_FAILURE,"Bad device name.");
 						}
 					// Getting current date
 					char date[9];
@@ -121,32 +127,28 @@ int main()
 					filename="/var/www/restfor/log/x1-"+device+"-"+std::string(date)+".log";
 					//filename="/home/ecogom/vigisystem/log/x1-"+device+"-"+std::string(date)+".log";
 					outputFile.open(filename.c_str(), std::ios::app);
+					if(!outputFile.is_open())
+						exitWell(EXIT_FAILURE,"Failed opening file '"+filename+"'.");
 					syslog(LOG_INFO, std::string("Opening file '"+filename+"'.").c_str());
 					}
 				// Removing output and input values
 				pos=line.find_last_of(',');
 				if(pos==std::string::npos)
 					{
-					syslog(LOG_INFO, std::string(device+": No comma found in line '"+line+"', case 3.").c_str());
-					closelog();
-					exit(EXIT_FAILURE);
+					exitWell(EXIT_FAILURE,"No comma found in line '"+line+"', case 3.");
 					}
 				line = line.substr(0,pos);
 				pos=line.find_last_of(',');
 				if(pos==std::string::npos)
 					{
-					syslog(LOG_INFO, std::string(device+": No comma found in line '"+line+"', case 4.").c_str());
-					closelog();
-					exit(EXIT_FAILURE);
+					exitWell(EXIT_FAILURE,"No comma found in line '"+line+"', case 4.");
 					}
 				line = line.substr(0,pos);
 				// Removing datetime given by the device
 				pos=line.find(',');
 				if(pos==std::string::npos)
 					{
-					syslog(LOG_INFO, std::string(device+": No comma found in line '"+line+"', case 5.").c_str());
-					closelog();
-					exit(EXIT_FAILURE);
+					exitWell(EXIT_FAILURE,"No comma found in line '"+line+"', case 5.");
 					}
 				line = line.substr(0,pos) + line.substr(pos+15,line.length()-1);
 				// Time is in the line, maybe trust this information is better ?
@@ -155,23 +157,22 @@ int main()
 					char curtime[9];
 					time_t timestamp = time(NULL);
 					strftime(curtime, sizeof(curtime), "%X", localtime(&timestamp));
+					if(!outputFile.is_open())
+						exitWell(EXIT_FAILURE,"Failed writing to file.");
 					outputFile << std::string(curtime) << "," << line << std::endl;
 					syslog(LOG_INFO, std::string("Logging "+line+".").c_str());
 					continue;
 					}
 				else
 					{
-					syslog(LOG_INFO, std::string("Not logged "+line+".").c_str());
-					exit(EXIT_FAILURE);
+					exitWell(EXIT_FAILURE,"Not logged "+line+".");
 					}
 				}
 			}
 		}
 	else
 		{
-		syslog(LOG_INFO, std::string(device+": Read timed out").c_str());
-		closelog();
-		exit(EXIT_FAILURE);
+		exitWell(EXIT_FAILURE, std::string(device+": Read timed out").c_str());
 		}
 	// Exit
 	return EXIT_SUCCESS;
